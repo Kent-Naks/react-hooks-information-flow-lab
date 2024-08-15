0593fd6593e1263b91a5a3bd04b15c20adf0135c
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-08-15 23:58:17 +0300

    Adds

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,16 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
-  function onDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+  const handleDarkModeClick = () => {
+    setDarkMode(prevMode => !prevMode);
+  };
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
+    <div className={darkMode ? 'dark-mode' : ''}>
+      <Header onDarkModeClick={handleDarkModeClick} />
+      <ShoppingList />
     </div>
   );
 }
-
-export default App;

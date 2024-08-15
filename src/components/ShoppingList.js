daef20b95df930a995da65f66a17a93ac68683f3
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-08-16 00:02:31 +0300

    Adds

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,31 +1,24 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter"; // Import the Filter component
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [category, setCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+  };
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+  const filteredItems = items.filter((item) => {
+    if (category === "All") return true;
+    return item.category === category;
   });
 
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+      <Filter onCategoryChange={handleCategoryChange} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
